5a4a0fda324e3d290d23032ae4a994cf544345ec
xiaobingcom <1043263723@qq.com>
2019-01-09 09:13:38 +0800

    更改注释

--- src/main/java/com/qingguomama/controller/UploadController.java
+++ src/main/java/com/qingguomama/controller/UploadController.java
@@ -1,62 +1,48 @@
 package com.qingguomama.controller;
 
-import com.avos.avoscloud.AVException;
-import com.avos.avoscloud.AVObject;
-import com.avos.avoscloud.SaveCallback;
 import com.qingguomama.service.UploadService;
-import com.qingguomama.util.LeanCloudUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
+@CrossOrigin(origins = {"http://localhost:2442"})
 public class UploadController {
     @Autowired
     private UploadService uploadService;
-    @Value("${qiniu.bucket.host.name}")
-    private String bucketHostName;
 
-    private String fileUrl;
-    @RequestMapping("fileUpload")
-    public String fileUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request){
-        LeanCloudUtil.start();
-        String currentUser = request.getParameter("CurrentUser");
+    private static String bucketHostName="pka9l5dgc.bkt.clouddn.com";
+
+    private static String fileUrl;
 
 
-        String imgUrl = fileUrl;
-
-
-
-        if (file!=null){
-
+    @RequestMapping(value="/fileUpload")
+    @ResponseBody
+    public String buttonTest(@RequestParam("file") MultipartFile file, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html;charset=utf-8");
+        /* 设置响应头允许ajax跨域访问 */
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        /* 星号表示所有的异域请求都可以接受， */
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+        if(file!=null) {
             String key = uploadService.upload(file);
-            imgUrl=bucketHostName+key;
-        }
+            String URL = bucketHostName + "/" + key;
 
-        AVObject user = new AVObject("User");
-        user.put("currentUser", currentUser);
-
-        AVObject Img = new AVObject("Imag");
-        Img.put("Url", fileUrl);
-
-        user.put("defaultImag", imgUrl);
-
-        user.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(AVException e) {
-                if (e == null) {
-
-                }
-            }
-        });
+            System.out.println(URL);
+            return URL;
+        }else{
+            return "false";
+        }//返回值给微信小程序
 
 
-        return imgUrl;
     }
 
 
--- src/main/java/com/qingguomama/service/impl/UploadServiceImpl.java
+++ src/main/java/com/qingguomama/service/impl/UploadServiceImpl.java
@@ -10,56 +10,52 @@ import com.qiniu.storage.Configuration;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.UUID;
 
 @Service
 public class UploadServiceImpl implements UploadService {
 
     //引入第一步的七牛配置
-    @Value("${qiniu.access.key}")
-    private String accesskey;
 
-    @Value("${qiniu.secret.key}")
-    private String secretKey;
+    private String accesskey="LGLAi-DeN-8TaVtmT4ajlwoB5ubhqJDnCbxros4H";
 
-    @Value("${qiniu.bucket.name}")
-    private String bucketName;
 
-    @Value("${qiniu.bucket.host.name}")
-    private String bucketHostName;
+    private String secretKey="R-QBk3oTg1aSW9OBFt6OyEOxhueh1EH3fkNvfxxM";
+
+
+    private String bucketName="qingguomamazuo";
+
+
+    private String bucketHostName="pka9l5dgc.bkt.clouddn.com";
 
 
 
     public String upload(MultipartFile image) throws UploadException {
-//构造一个带指定Zone对象的配置类
+///构造一个带指定Zone对象的配置类
         Configuration cfg = new Configuration(Zone.zone0());
 //...其他参数参考类注释
-
         UploadManager uploadManager = new UploadManager(cfg);
 //...生成上传凭证，然后准备上传
 
-
 //默认不指定key的情况下，以文件内容的hash值作为文件名
-        String key = UUID.randomUUID().toString()+System.nanoTime()+".jpg";
-
+        String key = UUID.randomUUID().toString()+ new Date().getMinutes()+".jpg";
         try {
-            byte[] uploadBytes = image.getBytes();
+            byte[] uploadBytes =image.getBytes();
             Auth auth = Auth.create(accesskey, secretKey);
             String upToken = auth.uploadToken(bucketName);
-
             try {
                 Response response = uploadManager.put(uploadBytes, key, upToken);
                 //解析上传成功的结果
                 DefaultPutRet putRet = new Gson().fromJson(response.bodyString(), DefaultPutRet.class);
                 System.out.println(putRet.key);
-                System.out.println(putRet.hash);
-               return key;
-            } catch (QiniuException ex) {
 
+            } catch (QiniuException ex) {
                 Response r = ex.response;
                 System.err.println(r.toString());
                 try {
@@ -68,8 +64,10 @@ public class UploadServiceImpl implements UploadService {
                     //ignore
                 }
             }
-        } catch (Exception ex) {
+        } catch (UnsupportedEncodingException ex) {
             //ignore
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return key;
     }
